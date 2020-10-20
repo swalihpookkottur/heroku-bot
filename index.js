d0a49a737c3455f6c3ee7347a52ddfcd305ec1bb
EAkbarshoh <72973070+EAkbarshoh@users.noreply.github.com>
2020-10-21 04:47:35 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-const PORT = process.env.PORT || 80
+const port = process.env.PORT || 80;
 
 
   const TelegramBot = require("node-telegram-bot-api");
@@ -17,19 +17,23 @@ const PORT = process.env.PORT || 80
     console.log('Bot has been started ....');
     bot.on('message', function (msg) {
             chatid = msg.chat.id;
-             bot.sendMessage(chatid,"hi"
+        
             if (msg.new_chat_member != null || msg.left_chat_member != null){
-                bot.sendMessage(chatid, JSON.stringify(msg, null, 4));
+                
                 bot.deleteMessage(chatid, msg.message_id);
-                bot.sendMessage(chatid,'Assalomu alaykum'+msg.new_chat_member.first_name,{
-                    MessageEntity:{
-                       
-                    }
-                }) ;
-            }
+                if (msg.new_chat_member.is_bot== true){
+                  for(let i=0;i<msg.new_chat_members.length;i++)
+                  bot.kickChatMember(chatid,msg.new_chat_members[i].id)
+                }
+                  bot.sendMessage(chatid, JSON.stringify(msg, null, 4));
+                }
+              
+                 
             
+    
             if ( msg.left_chat_member != null){
-                bot.sendMessage(chatid, JSON.stringify(msg, null, 4));
+
+               
                 bot.deleteMessage(chatid, msg.message_id);
             
             }
