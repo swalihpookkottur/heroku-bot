2a3c31a731802488ada29c10bad1041bd04ac6f3
EAkbarshoh <72973070+EAkbarshoh@users.noreply.github.com>
2020-10-20 19:09:09 +0500

    Not

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 80
     console.log('Bot has been started ....');
     bot.on('message', function (msg) {
             chatid = msg.chat.id;
-        
+             bot.sendMessage(chatid,"hi"
             if (msg.new_chat_member != null || msg.left_chat_member != null){
                 bot.sendMessage(chatid, JSON.stringify(msg, null, 4));
                 bot.deleteMessage(chatid, msg.message_id);
